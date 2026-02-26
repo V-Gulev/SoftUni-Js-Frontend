82d808ac4406941fdca63cc0eb18f651380092cf
V-Gulev <{velizargulev@gmail.com}>
2026-02-26 22:56:20 +0200

    Add calculate function to perform addition and log result

---
+++ JsSyntaxFundamentals/Lab/a-function.js
@@ -0,0 +1,12 @@
+function calculate (firstnum, secondnum) {
+    let result = firstnum + secondnum;
+    // console.log('The result is: ' + result);
+    // console.log(`The result is ${result}`);  
+    console.log(`The result is ${result.toFixed(1)}`);  
+    
+
+}
+
+calculate(5, 5);
+calculate(10, 20);
+calculate(1.45, 2.34);
