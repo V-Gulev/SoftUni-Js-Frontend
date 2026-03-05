c40bdfa5ed3134483f7d410643f80eca4d0e93c5
V-Gulev <velizargulev@gmail.com>
2026-03-05 20:58:50 +0200

    feat: Add solve functions for summing numbers and multiplication table

---
+++ JsSyntaxFundamentals/MoreExercise/Task04.js
@@ -0,0 +1,13 @@
+function solve(start, end) {
+    let result = "";
+    let sum = 0;
+
+    for (let i = start; i <= end; i++) {
+        result += i + " ";
+        sum += i;
+    }
+    console.log(result.trimEnd());
+    console.log(`Sum: ${sum}`);
+}
+
+solve(5, 10);
