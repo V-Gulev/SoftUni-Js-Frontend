895eb380693fe5d1218cf7976d386ed2a0949ca9
V-Gulev <velizargulev@gmail.com>
2026-04-01 22:13:16 +0300

    feat: Add HTML, CSS, and JavaScript files for "Show More" functionality

---
+++ DomIntroduction/Lab/04.Show More/show-more.js
@@ -0,0 +1,7 @@
+function showText() {
+    let textEl = document.getElementById('text');
+    let moreEl = document.getElementById('more');
+
+    moreEl.style.display = 'none';
+    textEl.style.display = 'inline';
+}
